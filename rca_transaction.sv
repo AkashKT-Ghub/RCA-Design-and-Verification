4654473ba75b788b607d1f7824bb2b959aa27d3c
Akash K T <ktakash02@gmail.com>
2025-11-09 00:31:03 +0530

    rca_transaction.sv

--- rca_transaction.sv
+++ rca_transaction.sv
@@ -1,8 +1,7 @@
 //RCA Transaction
 class adder_transaction extends uvm_sequence_item;
   
-  
-  `uvm_object_utils_begin(adder_transaction)
+  `uvm_object_utils_begin(adder_transaction) //FACTORY REGISTRATION
   `uvm_field_int(A,UVM_ALL_ON)
   `uvm_field_int(B,UVM_ALL_ON)
   `uvm_field_int(Cin,UVM_ALL_ON)
