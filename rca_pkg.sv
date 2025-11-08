ece5182622ffb752fa9fefdcf1323356f5b97bb9
Akash K T <ktakash02@gmail.com>
2025-11-09 00:38:28 +0530

    rca_pkg.sv
    RCA Package in SV

---
+++ rca_pkg.sv
@@ -0,0 +1,12 @@
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+
+`include "interface.sv"
+`include "transection.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "agent.sv"
+`include "scoreboard.sv"
+`include "env.sv"
+`include "test.sv"
